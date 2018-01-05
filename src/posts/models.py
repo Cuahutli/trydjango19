a2597f036e7a896540b2a5395db19859ee0250e0
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-01-05 13:00:58 -0700

    Fix problema de indentación

--- src/posts/models.py
+++ src/posts/models.py
@@ -15,15 +15,15 @@ class PostManager(models.Manager):
 
 def upload_location(instance, filename):
 	"""
-    instance.__class__ gets the model Post. We must use this method because the model is defined below.
-    Then create a queryset ordered by the "id"s of each object, 
-    Then we get the last object in the queryset with `.last()`
-    Which will give us the most recently created Model instance
-    We add 1 to it, so we get what should be the same id as the the post we are creating.
-    """
-    PostModel = instance.__class__
-    new_id = PostModel.objects.order_by("id").last().id + 1
-    return "%s/%s" %(new_id, filename)
+	instance.__class__ gets the model Post. We must use this method because the model is defined below.
+	Then create a queryset ordered by the "id"s of each object, 
+	Then we get the last object in the queryset with `.last()`
+	Which will give us the most recently created Model instance
+	We add 1 to it, so we get what should be the same id as the the post we are creating.
+	"""
+	PostModel = instance.__class__
+	new_id = PostModel.objects.order_by("id").last().id + 1
+	return "%s/%s" %(new_id, filename)
 
 class Post(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
